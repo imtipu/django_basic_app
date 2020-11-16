1dfdd80286cbc1081e3bd2b831e554de47c14f25
Borhan Tipu <me@imtipu.me>
2020-11-16 21:34:53 +0600

    Login Required Mixin added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', dashboard_home, name="dashboard_home")
+    path('', dashboard_home, name="dashboard_home"),
+    path('users/', UserListView.as_view(), name='user_list')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
-
 # Create your views here.
+from django.views.generic import ListView, View, TemplateView
+
+User = get_user_model()
+
+
+class SuperUserLoginRequiredMixin(LoginRequiredMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
 
 @login_required(login_url='/accounts/login')
 def dashboard_home(request):
@@ -10,3 +22,13 @@ def dashboard_home(request):
         'title': 'Dashboard'
     }
     return render(request, 'dashboard/index.html', data)
+
+
+class UserListView(SuperUserLoginRequiredMixin, ListView):
+    template_name = 'dashboard/user_list.html'
+    context_object_name = 'users'
+    model = User
+
+# class TestView(TemplateView):
+#     def get(self, request, *args, **kwargs):
+#
